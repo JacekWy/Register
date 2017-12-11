94f6dbe0a092be9048e714a0ed291980285d2015
JacekWy <34398043+JacekWy@users.noreply.github.com>
2017-12-11 16:01:26 +0100

    Zmiana

--- Register.php
+++ Register.php
@@ -1,1 +1,21 @@
 <?php
+require_once ('Connect.php');
+
+
+$user = $_POST['User'];
+$pass1 = $_POST['password'];
+$pass2 = $_POST['passwordr'];
+
+$conn->query("insert into uzytkownicy (User, Password, PasswordR) VALUES ('$user','$pass1','$pass2')");
+$conn->close();
+
+
+
+echo 'User: '.$user.'<br>';
+echo 'Password1: '.$pass1.'<br>';
+echo 'Password2: '.$pass2.'<br>';
+
+
+?>
+
+
