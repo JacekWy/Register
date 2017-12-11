5f0dc3ff19e244a71b22e8afbb6b7d9b0d1fb60b
JacekWy <34398043+JacekWy@users.noreply.github.com>
2017-12-11 16:39:47 +0100

    Zmiana

--- OsadnicyGame.php
+++ OsadnicyGame.php
@@ -1,6 +1,19 @@
 <?php
+require_once ('Connect.php');
 
+echo 'Zalogowwny'.'<br>';
+echo 'Twoje konto'.'<br>';
 
-echo 'Zalogowwny';
+$result = $conn->query("select * from magazyn");
 
-echo 'Twoje konto';
+if($result->num_rows > 0)
+{
+    while ($row = $result->fetch_assoc())
+    {
+       echo 'Masz: '.$row['Wood'].' Drewna'.'<br>';
+       echo 'Masz: '.$row['Food'].' Jedzenia'.'<br>';
+       echo 'Masz: '.$row['Iron'].' Zelaza'.'<br>';
+
+    }
+}
+
